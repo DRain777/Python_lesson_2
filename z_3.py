cd36038ad7e092c134ac93bcf43547486b424c42
Denis <d.cochew@yandex.ru>
2022-11-28 14:33:45 +0700

    summa

--- z_3.py
+++ z_3.py
@@ -3,11 +3,16 @@
 num = int(input("Введите число от 0 до 9: "))
 if num == 0:# проверка введеного числа
     result = 0
+    print(result)
 elif num > 9:
-    result = "Вы ввели число больше 9"
+    result_1 = "Вы ввели число больше 9"
+    print(result_1)
+   
 
 else:
     num_2 = num * 10 + num
     num_3 = num_2 * 10 + num
     summa = num + num_2 + num_3
     print(f' {num} + {num_2} + {num_3} = {summa}')
+    
+   
